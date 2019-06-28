e51330738c99eff663e64b72a9def744304dc996
jonathan cho <jonathan.h.cho@jpl.nasa.gov>
2019-06-28 10:51:38 -0700

    fix build prod bugs

--- src/app/shared/pipes/escape-html.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EscapeHtmlPipe } from './escape-html.pipe';
-
-describe('EscapeHtmlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EscapeHtmlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
